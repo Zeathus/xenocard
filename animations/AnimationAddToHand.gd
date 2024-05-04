9821091a8f5abbf8495bcb7ba4fadc595eed75c7
Zeathus <zeathus@gmail.com>
2024-05-04 23:02:36 +0200

    Erroneously didn't add new animation files to last commit

---
+++ animations/AnimationAddToHand.gd
@@ -0,0 +1,61 @@
+extends GameAnimation
+
+class_name AnimationAddToHand
+
+var node: Node2D
+var original_distance: float
+var destination: Vector2
+var original_scale: Vector2
+var target_scale: Vector2
+var speed: float
+var time: float
+var max_time: float = 0.5
+var hand: GameHand
+var hand_move: float
+var hand_original_x_position: float
+
+func _init(node: Node2D, hand: GameHand, speed_mod: float = 1.0):
+	self.node = node
+	self.hand = hand
+	self.max_time /= speed_mod
+	self.time = 0
+	var original_position: Vector2 = node.global_position
+	destination = hand.get_new_card_position()
+	hand_move = 64 if hand.global_rotation == 0 else -64
+	if hand.size() >= 5:
+		hand_move *= 2
+	hand_original_x_position = hand.position.x
+	hand.position.x += hand_move
+	original_scale = node.scale
+	target_scale = node.scale
+	node.global_position = original_position
+	if node.has_method("set_in_motion"):
+		node.set_in_motion(true)
+
+func update(delta):
+	if is_done():
+		return
+	if time == 0:
+		original_distance = destination.distance_to(node.global_position)
+		speed = (original_distance / max_time)
+	time += delta
+	var distance: Vector2 = destination - node.global_position
+	var move: Vector2 = distance.normalized() * speed * delta
+	if move.length() >= distance.length():
+		node.global_position = destination
+		node.scale = target_scale
+		hand.position.x = hand_original_x_position
+		if node.has_method("set_in_motion"):
+			node.set_in_motion(false)
+		finish()
+	else:
+		node.global_position += move
+		node.scale = original_scale * (distance.length() / original_distance) + \
+			target_scale * ((original_distance - distance.length()) / original_distance)
+		hand.position.x = hand_original_x_position + hand_move * (distance.length() / original_distance)
+	if time >= 2.0:
+		node.scale = target_scale
+		hand.position.x = hand_original_x_position
+		if node.has_method("set_in_motion"):
+			node.set_in_motion(false)
+		finish()
---
+++ animations/AnimationAttack.gd
@@ -0,0 +1,42 @@
+extends GameAnimation
+
+class_name AnimationAttack
+
+static var projectile_scene = preload("res://animations/projectile.tscn")
+var attacker: Node2D
+var targets: Array[Node2D]
+var projectiles: Array[Node2D]
+var time: float
+var move_end_time: float = 1.0
+
+func _init(attacker: Node2D, targets: Array[Node2D]):
+	self.attacker = attacker
+	self.targets = targets
+	self.projectiles = []
+	self.time = 0
+
+func make_projectile(target: Node2D):
+	var p: Node2D = projectile_scene.instantiate()
+	attacker.add_child(p)
+	p.global_position = attacker.global_position
+	p.scale = Vector2(1.0, 1.0) / attacker.scale
+	projectiles.push_back(p)
+
+func update(delta):
+	if is_done():
+		return
+	if time == 0:
+		for t in targets:
+			make_projectile(t)
+	time += delta
+	var move_progress: float = min(time, move_end_time)
+	for i in range(len(targets)):
+		var t = targets[i]
+		var p = projectiles[i]
+		p.global_position = t.global_position * (move_progress / move_end_time) + \
+			attacker.global_position * ((move_end_time - move_progress) / move_end_time)
+	if time >= move_end_time:
+		for p in projectiles:
+			attacker.remove_child(p)
+			p.queue_free()
+		finish()
