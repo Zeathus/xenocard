a4698975632da38534f8c029a8dd3d2841c14312
Zeathus <zeathus@gmail.com>
2024-10-30 18:55:24 +0100

    Shion and Ziggy

--- effects/effects/EffectLevelUpIgnoreReq.gd
+++ effects/effects/EffectLevelUpIgnoreReq.gd
@@ -3,7 +3,7 @@ extends EffectLevelUp
 class_name EffectLevelUpIgnoreReq
 
 func get_field_requirements(req: Array[Enum.Attribute]) -> Array[Enum.Attribute]:
-	for c in card.owner.field.get_battlefield_cards():
-		if filter.is_valid(card.owner, c):
+	for c in parent.host.owner.field.get_battlefield_cards():
+		if filter.is_valid(parent.host.owner, c):
 			return []
 	return super.get_field_requirements(req)
--- effects/effects/EffectSearch.gd
+++ effects/effects/EffectSearch.gd
@@ -7,5 +7,5 @@ var filter: CardFilter
 func post_init():
 	filter = CardFilter.new(param)
 
-func effect():
-	events.push_back(EventSearch.new(game_board, card.owner, filter, "Select a card to add to your hand"))
+func effect(variables: Dictionary = {}):
+	parent.events.push_back(EventSearch.new(game_board, parent.host.owner, filter, "Select a card to add to your hand"))
