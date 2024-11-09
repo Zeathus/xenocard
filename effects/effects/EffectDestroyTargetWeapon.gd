ef2f6ec68f1439b80768d8cc80d7de741f7737cf
Zeathus <zeathus@gmail.com>
2024-11-09 13:04:41 +0100

    Implement Phase Transition Cannon, Precise Shooting and Premonition of Gnosis

---
+++ effects/effects/EffectDestroyTargetWeapon.gd
@@ -0,0 +1,21 @@
+extends Effect
+
+class_name EffectDestroyTargetWeapon
+
+var filter: CardFilter
+
+func post_init():
+	filter = CardFilter.new(param)
+
+func targets_to_select_for_effect() -> Array[CardFilter]:
+	return [filter]
+
+func effect(variables: Dictionary = {}):
+	for t in variables["effect_targets"]:
+		parent.events.push_back(EventDestroy.new(parent.get_game_board(), parent.host, t.equipped_weapon, Damage.new(Damage.EFFECT | Damage.DESTROY)))
+
+func has_valid_targets(variables: Dictionary = {}) -> bool:
+	for t in game_board.get_all_field_cards():
+		if filter.is_valid(parent.host.owner, t, variables) and t.equipped_weapon:
+			return true
+	return false
---
+++ effects/effects/EffectDestroyWeapon.gd
@@ -0,0 +1,14 @@
+extends Effect
+
+class_name EffectDestroyWeapon
+
+var filter: CardFilter = null
+
+func post_init():
+	filter = CardFilter.new(param)
+
+func effect(variables: Dictionary = {}):
+	for c in parent.get_game_board().get_all_field_cards():
+		if filter.is_valid(parent.host.owner, c, variables):
+			parent.events.push_back(EventDestroy.new(parent.get_game_board(), parent.host, c.equipped_weapon, Damage.new(Damage.EFFECT | Damage.DESTROY)))
+			c.unequip()
