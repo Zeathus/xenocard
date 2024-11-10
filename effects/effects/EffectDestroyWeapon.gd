94681582b178eafb466f4abb97abb8401dbd65e8
Zeathus <zeathus@gmail.com>
2024-11-10 18:45:21 +0100

    Implement Resolution of the Assembly

--- effects/effects/EffectDestroyWeapon.gd
+++ effects/effects/EffectDestroyWeapon.gd
@@ -9,6 +9,12 @@ func post_init():
 
 func effect(variables: Dictionary = {}):
 	for c in parent.get_game_board().get_all_field_cards():
-		if filter.is_valid(parent.host.owner, c, variables):
+		if filter.is_valid(parent.host.owner, c, variables) and c.equipped_weapon:
 			parent.events.push_back(EventDestroy.new(parent.get_game_board(), parent.host, c.equipped_weapon, Damage.new(Damage.EFFECT | Damage.DESTROY)))
 			c.unequip()
+
+func has_valid_targets(variables: Dictionary = {}) -> bool:
+	for t in game_board.get_all_field_cards():
+		if filter.is_valid(parent.host.owner, t, variables) and t.equipped_weapon:
+			return true
+	return false
