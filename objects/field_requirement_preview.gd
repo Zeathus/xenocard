900c85c96aff6bee939711e80d9dfdfdf8eb6ec2
Zeathus <zeathus@gmail.com>
2024-12-03 05:55:57 +0100

    Add a preview for what field requirements are met when hovering over cards in the hand

---
+++ objects/field_requirement_preview.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func set_requirement_and_resources(requirement: Array[Enum.Attribute], resources: Array[Enum.Attribute]) -> void:
+	resources = resources.duplicate()
+	var attributes: Array[Node2D] = [$Panel/Attribute1, $Panel/Attribute2, $Panel/Attribute3, $Panel/Attribute4]
+	if len(requirement) == 0:
+		for i in attributes:
+			i.visible = false
+		$Panel/LabelNone.visible = true
+		$Panel.size.x = 330
+		$Panel.position.x = -$Panel.size.x / 2
+		return
+	else:
+		$Panel.size.x = 276 + 88 * (len(requirement) - 1)
+		$Panel/LabelNone.visible = false
+		$Panel.position.x = -$Panel.size.x / 2
+	for i in range(4):
+		if len(requirement) <= i:
+			attributes[i].visible = false
+		else:
+			attributes[i].set_attribute(requirement[i])
+			attributes[i].visible = true
+			var yes_node: Label = attributes[i].find_child("Yes")
+			var no_node: Label = attributes[i].find_child("No")
+			if requirement[i] in resources:
+				yes_node.visible = true
+				no_node.visible = false
+				resources.erase(requirement[i])
+			elif requirement[i] == Enum.Attribute.ANY and len(resources) > 0:
+				yes_node.visible = true
+				no_node.visible = false
+				resources.pop_back()
+			else:
+				yes_node.visible = false
+				no_node.visible = true
--- scenes/game_hand.gd
+++ scenes/game_hand.gd
@@ -4,6 +4,7 @@ class_name GameHand
 
 var cards: Array[Card]
 var game_board: GameBoard
+var hovered_card: Card
 
 func _ready():
 	cards = []
@@ -35,7 +36,17 @@ func _process(delta):
 			card.instance.position.y = -64 * (128 - distance) / 128
 		else:
 			card.instance.position.y = 0
-			
+	if hovered_card:
+		if hovered_card.is_hovered() and hovered_card in cards:
+			if global_rotation == 0:
+				$FieldRequirementPreview.global_position = hovered_card.instance.global_position - Vector2(0, 140)
+				$FieldRequirementPreview.rotation = 0
+			else:
+				$FieldRequirementPreview.global_position = hovered_card.instance.global_position + Vector2(0, 140)
+				$FieldRequirementPreview.rotation = -global_rotation
+		else:
+			hide_field_requirements()
+			hovered_card = null
 
 func add_card(card: Card):
 	card.reset()
@@ -43,10 +54,12 @@ func add_card(card: Card):
 	cards.push_back(card)
 	add_child(card.instance)
 	card.instance.selected.connect(_on_card_selected)
+	card.instance.on_hover.connect(_on_card_hovered)
 	self.refresh()
 
 func remove(card: Card):
 	card.instance.selected.disconnect(_on_card_selected)
+	card.instance.on_hover.disconnect(_on_card_hovered)
 	remove_child(card.instance)
 	cards.erase(card)
 	self.refresh()
@@ -65,6 +78,22 @@ func get_new_card_position() -> Vector2:
 		return global_position + Vector2(min(self.size(), 4) * 64 - 64, 0)
 	else:
 		return global_position - Vector2(min(self.size(), 4) * 64 - 64, 0)
+
+func show_field_requirements():
+	$FieldRequirementPreview.set_requirement_and_resources(
+		hovered_card.get_field_requirements(),
+		hovered_card.owner.get_resources()
+	)
+	$FieldRequirementPreview.visible = true
+
+func hide_field_requirements():
+	$FieldRequirementPreview.visible = false
+
+func _on_card_hovered(card: Card):
+	if not card.instance or card.instance.is_face_down():
+		return
+	hovered_card = card
+	show_field_requirements()
 
 func _on_card_selected(card: Card):
 	game_board.on_hand_card_selected(self, card)
