7c413664c38c3ddfd6ed2fb36d26c7a8d7b8c3ca
Zeathus <zeathus@gmail.com>
2025-01-26 22:54:15 +0100

    Progress on Playing Cards tutorial

--- tutorial/tutorials/Tutorial8PlayingCards.gd
+++ tutorial/tutorials/Tutorial8PlayingCards.gd
@@ -18,10 +18,28 @@ func is_playable() -> bool:
 	return true
 
 func get_hint(turn_count: int, event: Event) -> TutorialHint:
-	if event is EventPhaseSet:
-		return TutorialHint.new(0, "Try right clicking cards\nto get information about them.\nClick End Phase on the left when done.")
-	elif event is EventPhaseEventBlock:
-		var hint: TutorialHint = TutorialHint.new(0, "Try right clicking cards different\ncards to get information about them.\nClick End Phase on the left when done.")
-		hint.exec = complete_tutorial
-		return hint
+	match turn_count:
+		3:
+			if event is EventPhaseEventBlock and event.player.id == 0:
+				return TutorialHint.new(0, "This is your event phase.\nYou do not have the field\nrequirements for Mary and Shelley.\nClick \"End Phase\".")
+			elif event is EventPhaseSet:
+				return TutorialHint.new(0, "This is your set phase.\nYou can now play one battle card\nand one situation card.\nYou cannot play Stim yet as you\ndo not have its field requirements.\nPlay a card by clicking it,\nthen a free standby zone.")
+		4:
+			if event is EventPhaseEventBlock and event.player.id == 0:
+				return TutorialHint.new(0, "This is your block phase.\nYou still cannot play any event cards.\nClick \"End Phase\".")
+		5:
+			if event is EventPhaseMove:
+				return TutorialHint.new(0, "This is your move phase.\nYou can move cards to or from\nthe battlefield.\nWhen done, click \"End Phase\".")
+			elif event is EventPhaseEventBlock and event.player.id == 0:
+				return TutorialHint.new(0, "Click \"End Phase\" to\ngo to your Set Phase.")
+			elif event is EventPhaseSet:
+				return TutorialHint.new(0, "You drew a Combat Realian Male.\nIt has the \"Unlimited\" effect,\nso you can play it the same\nturn as other battle cards.")
+		6:
+			if event is EventPhaseEventBlock and event.player.id == 0:
+				return TutorialHint.new(0, "You now meet the requirements\nfor Mary and Shelley.\nYou can play it now, or\nduring your next event phase.")
+			elif event is EventPhaseSet:
+				return TutorialHint.new(0, "You drew a Combat Realian Male.\nIt has the \"Unlimited\" effect,\nso you can play it the same\nturn as other battle cards.")
+			
+	#hint.exec = complete_tutorial
+	#return hint
 	return null
